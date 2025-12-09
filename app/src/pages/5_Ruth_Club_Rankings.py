2f37f88524187e182ab8908cd5244f59b86f5d97
SirIvanB <blinov.i@northeastern.edu>
2025-12-08 21:46:52 -0500

    date change

--- app/src/pages/5_Ruth_Club_Rankings.py
+++ app/src/pages/5_Ruth_Club_Rankings.py
@@ -40,7 +40,7 @@ with col1:
     )
 
 with col2:
-    st.markdown("### 📅 Current Period: 2024")
+    st.markdown("### 📅 Current Period: 2025")
 #    period = st.selectbox(
 #        "Select period:",
 #        options=["2024-Q4", "2024-Q3", "2024-Q2", "2024-Q1", "2025-Q1"],
